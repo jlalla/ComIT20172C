f04ef9ee4a77e99065dcda156bb6c744016ecaa9
lalla <juanlalla@gmail.com>
2017-06-30 10:21:29 -0300

    Se define la clase Usuario

--- Blog/Models/Usuario.cs
+++ Blog/Models/Usuario.cs
@@ -2,5 +2,10 @@
 {
     public class Usuario
     {
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Bio { get; set; }
     }
 }
