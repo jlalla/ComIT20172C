66d397d55305c5b957ccac3c7f44f2b2a578c719
lalla <juanlalla@gmail.com>
2017-06-30 10:11:28 -0300

    Agregamos clases de modelo de la aplicación

---
+++ Blog/Models/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    /// <summary>
+    /// Representa a la entidad artículo del blog
+    /// </summary>
+    public class Articulo
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public string Imagen { get; set; }
+        public Usuario Autor { get; set; }
+        public List<Comentario> Comentarios { get; set; }
+    }
+}
